1005b5945cf29133f9b008454078c582f55ccb12
binseo <oppfgh0901@naver.com>
2025-06-03 20:08:31 +0900

    .

--- cjfood/js/main.js
+++ cjfood/js/main.js
@@ -114,6 +114,27 @@ $(document).ready(function(){
 
 
 
+    /************************************* webzine 시작 ***********************************/
+
+    const webzine_swiper = new Swiper('.webzine .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: { 
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 2500,
+        //     disableOnInteraction: true,
+        // },
+    });
+
+
+    /************************************* webzine 종료 ***********************************/
+
 
 
 })
