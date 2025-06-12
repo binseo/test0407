fe62127329eda1d433b9b13183e740aca2cef595
binseo <oppfgh0901@naver.com>
2025-06-12 19:22:17 +0900

    .

--- animal/js/main.js
+++ animal/js/main.js
@@ -186,6 +186,30 @@ $(document).ready(function(){
 	/************************************ adopt swiper : 시작 ************************************/
 
 	const adopt_swiper = new Swiper('.adopt .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			768: {    /* 640px 이상일때 적용 */
+				spaceBetween: 24,
+				centeredSlides: true, /* pc일 때만 적용 */
+			},
+		},
+		centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+		
+		navigation: {
+			nextEl: '.adopt .list_ctrl .btn_next',
+            prevEl: '.adopt .list_ctrl .btn_prev',
+		},
+	});
+
+
+	/************************************ adopt swiper : 끝 ************************************/
+
+
+	/************************************ review swiper : 시작 ************************************/
+
+	const review_swiper = new Swiper('.review .swiper', { /* 팝업을 감싼는 요소의 class명 */
 		slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
 		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
 		breakpoints: {
@@ -197,23 +221,16 @@ $(document).ready(function(){
 		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
 		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 		autoplay: {  /* 팝업 자동 실행 */
-			delay: 9000,
+			delay: 2500,
 			disableOnInteraction: true,
 		},
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		},
-		pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-			el: '.swiper-pagination', /* 해당 요소의 class명 */
-			clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-			type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-		},
 	});
-	adopt_swiper.autoplay.stop();  /* 일시정지 기능 */
-	adopt_swiper.autoplay.start();  /* 재생 기능 */
 
 
-	/************************************ adopt swiper : 끝 ************************************/
+	/************************************ review swiper : 끝 ************************************/
 
 })
