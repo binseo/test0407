46f4d3c07c9a4d0554e51406cf6e482f55202dc5
binseo <oppfgh0901@naver.com>
2025-06-13 09:32:09 +0900

    .

--- animal/js/main.js
+++ animal/js/main.js
@@ -210,27 +210,35 @@ $(document).ready(function(){
 	/************************************ review swiper : 시작 ************************************/
 
 	const review_swiper = new Swiper('.review .swiper', { /* 팝업을 감싼는 요소의 class명 */
-		slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
 		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
 		breakpoints: {
-			640: {    /* 640px 이상일때 적용 */
+			768: {    /* 640px 이상일때 적용 */
 				slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-				spaceBetween: 20,
+				spaceBetween: 24,
+			},
+			1024: {    /* 640px 이상일때 적용 */
+				slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
 			},
 		},
 		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
 		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-		autoplay: {  /* 팝업 자동 실행 */
-			delay: 2500,
-			disableOnInteraction: true,
-		},
+		// autoplay: {  /* 팝업 자동 실행 */
+		// 	delay: 2500,
+		// 	disableOnInteraction: true,
+		// },
 		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
+			nextEl: '.review .list .btn_next',
+            prevEl: '.review .list .btn_prev',
 		},
 	});
 
-
 	/************************************ review swiper : 끝 ************************************/
+
+
+	/************************************ footer top 버튼 :: 시작 ************************************/
+
+	/************************************ footer top 버튼 ::  끝 ************************************/
 
 })
